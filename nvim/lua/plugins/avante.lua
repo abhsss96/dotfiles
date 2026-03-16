e816416099113546889de1fd5c5b75e6e0b1b3da
Abhishek Sharma <abhsss96@gmail.com>
2026-03-16 12:03:48 +0530

    Lazy update.

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -6,13 +6,17 @@ return {
     -- add any opts here
     -- for example
     provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    providers = {
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        extra_request_body = {
+          temperature = 0,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -24,7 +28,7 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-mini/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -2,6 +2,7 @@ return {
   "abhsss96/notes-nvim",
   lazy = true,
   cmd = { "Note", "NoteToday", "NoteIndex", "NoteFind", "NoteFindAll", "NoteGrep", "NoteRename", "NoteBrowse" },
+  keys = { { "<leader>n" } },
   opts = {
     notes_dir = "~/notes",
     note_extension = ".md",
